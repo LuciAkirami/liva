01d2334a079639d706f17e9116615a598abf0c39
LuciAkirami <ajsubscriber001@gmail.com>
2024-02-25 16:45:26 +0530

    Removed Audio Classification pipeline

--- speech_processor.py
+++ speech_processor.py
@@ -3,9 +3,9 @@ from transformers import pipeline
 class SpeechProcessor:
     def __init__(self, device, stt_model = "openai/whisper-base.en"):
         self.stt_model = stt_model
-        self.classifier = pipeline(
-            "audio-classification", model="MIT/ast-finetuned-speech-commands-v2", device=device
-        )
+        # self.classifier = pipeline(
+        #     "audio-classification", model="MIT/ast-finetuned-speech-commands-v2", device=device
+        # )
         self.transcriber = pipeline(
             "automatic-speech-recognition", model=self.stt_model, device=device
         )
